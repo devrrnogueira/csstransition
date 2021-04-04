d9320aab4831000cba962691aed550fcd00d43e8
devrrnogueira <dev.rrnogueira@gmail.com>
2021-04-04 17:27:12 -0300

    fix package name

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: 'index.js',
-        library: 'mylibrary',
+        library: 'csstransition',
         libraryTarget: 'umd'
     },
     // plugins: [
